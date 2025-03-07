fd6c23181f23954168104e930404ef80e31d98a3
Lucas Sagas <lucas.sagas@rdstation.com>
2025-03-07 10:25:06 -0300

    Update hero section layout with new background image, logo integration, and improved text styling

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,18 +1,22 @@
 import { HeroForm } from './hero-form'
 import { motion } from 'framer-motion'
+import logo from '../../assets/footer/logo.png'
 
 export const Hero = () => {
   return (
-    <div className="w-full h-screen bg-blue-600 flex bg-[url(/hero.png)] bg-cover bg-center overflow-hidden">
-      <div className="w-full max-w-[1440px] mx-auto px-8 flex items-center justify-center py-10 flex-wrap md:justify-between">
-        <motion.h1
-          className="text-5xl text-center md:text-start max-w-[500px] text-white font-primary"
+    <div className="w-full md:h-screen bg-blue-600 flex bg-[url(/hero2.jpeg)] bg-center md:bg-cover bg-[0px_-150px] overflow-x-hidden">
+      <div className="w-full max-w-[1440px] mx-auto px-8 flex items-center justify-center py-10 flex-wrap md:justify-between gap-6">
+        <motion.div
+          className="flex flex-col gap-6 items-center"
           initial={{ opacity: 0, x: -200 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 1 }}
         >
-          O caminho mais seguro para suas grandes realizações!
-        </motion.h1>
+          <img src={logo} alt="logo" className="md:w-[250px] w-[150px]" />
+          <h1 className="md:text-5xl text-4xl text-center max-w-[500px] font-primary text-zinc-200">
+            O caminho mais seguro para suas grandes realizações!
+          </h1>
+        </motion.div>
         <HeroForm />
       </div>
     </div>
