aa9480311cb4a1fc3815d90347211451850f7b61
Lucas Sagas <lucas.sagas@rdstation.com>
2025-04-07 15:00:18 -0300

    adjust hero section height and reposition footer

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion'
 
 export const Hero = () => {
   return (
-    <div className="w-full flex bg-[url(/hero.png)] bg-cover overflow-x-hidden relative md:min-h-[800px] min-h-[1500px]">
+    <div className="w-full flex bg-[url(/hero.png)] bg-cover overflow-x-hidden relative md:min-h-[800px] min-h-[1100px]">
       <div className="absolute left-0 bottom-0 top-0 w-full h-[100%] opacity-95" />
       <div className="w-full px-8 flex items-center justify-center py-10 flex-wrap md:justify-around gap-6 z-10 relative">
         <motion.div
@@ -21,7 +21,7 @@ export const Hero = () => {
           </span>
         </motion.div>
         <HeroForm />
-        <div className=" absolute bottom-0 w-full h-[120px] flex">
+        <div className=" absolute md:bottom-0 bottom-[-100px] w-full h-[120px] flex">
           <span className="bg-linear-to-bl from-black from-60% to-zinc-700 to-95% w-[350px] h-full flex items-center justify-center text-4xl text-white border-2 border-zinc-600">
             Produtos
           </span>
