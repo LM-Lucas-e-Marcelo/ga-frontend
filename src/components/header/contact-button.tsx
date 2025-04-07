273c152c1e028c82f4372a606db1c30c82c4b4f7
Lucas Sagas <lucas.sagas@rdstation.com>
2025-04-07 14:52:44 -0300

    fix contact button

--- src/components/header/contact-button.tsx
+++ src/components/header/contact-button.tsx
@@ -2,7 +2,9 @@ export const ContactButton = () => {
   return (
     <div className=" h-[70px] pr-8 w-full md:flex hidden items-center bg-linear-to-bl from-zinc-400 from-20% to-white to-40%">
       <a
-        href="https://wa.me/48999999999"
+        href="https://wa.me/48998474237"
+        rel="noopener noreferrer"
+        target="_blank"
         className="bg-black text-white px-4 py-2 block w-[180px] text-center rounded-full"
       >
         Área do cliente
